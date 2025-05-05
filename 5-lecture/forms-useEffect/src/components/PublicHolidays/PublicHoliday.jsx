519f701d88e9a94f6b17e9e2b3f839b69ccfeba8
Barbora6 <smetanovabarbora@email.cz>
2025-05-05 20:07:06 +0200

    pomocí useEffectu jsem získala data z funkce fetchAndSetHolidays

--- 5-lecture/forms-useEffect/src/components/PublicHolidays/PublicHoliday.jsx
+++ 5-lecture/forms-useEffect/src/components/PublicHolidays/PublicHoliday.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 export const PublicHoliday = () => {
+  const [holidays, setHolidays] = useState();
+
+  useEffect(() => {
+    const fetchAndSetHolidays = async () => {
+      const response = await fetch(
+        "https://date.nager.at/api/v3/PublicHolidays/2025/CZ"
+      );
+      setHolidays(await response.json());
+    };
+
+    fetchAndSetHolidays();
+  }, []);
+
+  // [] pole závislostí
+  // useEffect nesmí být async funkce
+
   return (
-    <div>
-      <h1>Státní svátky roku 2025</h1>
-    </div>
+    <main>
+      <header>
+        <h1>Státní svátky roku 2025</h1>
+      </header>
+      <section>
+        {holidays === undefined ? (
+          <div>Data se načítají.</div>
+        ) : (
+          <ul>
+            {holidays.map((holiday) => (
+              <li>{holiday.localName}</li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
   );
 };
