3adeba6d465ee09636038c4f8877652c78cb52e5
Barbora6 <smetanovabarbora@email.cz>
2025-05-05 18:40:26 +0200

    první input křestní jméno, funkce handleFirstNameChange

--- 5-lecture/forms-useEffect/src/components/Forms.jsx
+++ 5-lecture/forms-useEffect/src/components/Forms.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 export const Forms = () => {
+  const [firstName, setFirstName] = useState("");
+
+  const handleFirstnameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
   return (
-    <div>
+    <main>
       <h1>Tady bude formulář</h1>
-    </div>
+      <table>
+        <tr>
+          <th>Křestní jméno</th>
+          <td>
+            <input
+              type="text"
+              value={firstName}
+              onChange={handleFirstnameChange}
+            />
+          </td>
+        </tr>
+      </table>
+    </main>
   );
 };
+
+// pro každé políčku musíme zavést useState, potom v inputu zadám jako hodnotu proměnnou z useState
+// přidám událost onChange a vrátím v ní setFirstName
