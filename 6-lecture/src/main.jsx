cb67aea688cd0fe513abc54af88f6bd1a4c2613e
Barbora6 <smetanovabarbora@email.cz>
2025-05-07 18:49:39 +0200

    import IntlProvider

--- 6-lecture/src/main.jsx
+++ 6-lecture/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { IntlProvider } from "react-intl";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <IntlProvider locale="cs" messages={messages.cs}>
     <App />
-  </StrictMode>,
-)
+  </IntlProvider>
+);
+
+const messages = {
+  cs: {
+    title: "Vícejazyčnost",
+    greeting: "Ahoj, zdravím tě z vícejazyčné aplikace.",
+    current_dateTime: "Aktuální čas"
+  },
+  en: {
+    title: "Internationalization",
+    greeting: "Hello from a multilingual application.",
+    current_dateTime: "Current time"
+  }
+};
